7c46e7940f55918c88e58578975a31aad5c9171b
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-05-22 21:58:35 +0530

    Basic Model setup with Auth Done

---
+++ ApparelShoppingAppSolution/ApparelShoppingAppAPI/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApparelShoppingAppAPI.Models.DB_Models;
+using ApparelShoppingAppAPI.Models.DTO_Models;
+using ApparelShoppingAppAPI.Services.Interfaces;
+using ApparelShoppingAppAPI.Exceptions;
+
+namespace ApparelShoppingAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Login")]
+        [ProducesResponseType(typeof(LoginReturnDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<LoginReturnDTO>> Login(UserLoginDTO userLoginDTO)
+        {
+            try
+            {
+                var result = await _userService.Login(userLoginDTO);
+                return Ok(result);
+            }
+            catch (UnauthorizedUserException ex)
+            {
+                return Unauthorized(new ErrorModel(401, ex.Message));
+            }
+            catch (NotAbelToLoginException ex)
+            {
+                return Unauthorized(new ErrorModel(401, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ErrorModel(500, $"An unexpected error occurred. {ex.Message}"));
+            }
+        }
+
+        [HttpPost("Register")]
+        [ProducesResponseType(typeof(RegisterReturnDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<RegisterReturnDTO>> Register(UserRegisterDTO userRegisterDTO)
+        {
+            try
+            {
+                var result = await _userService.Register(userRegisterDTO);
+                return Ok(result);
+            }
+            catch (UserAlreadyExistsException ex)
+            {
+                return BadRequest(new ErrorModel(400, ex.Message));
+            }
+            catch (PasswordMismatchException ex)
+            {
+                return BadRequest(new ErrorModel(400, ex.Message));
+            }
+            catch (UnableToRegisterException ex)
+            {
+                return BadRequest(new ErrorModel(400, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ErrorModel(500, $"An unexpected error occurred. {ex.Message}"));
+            }
+        }
+    }
+}
---
+++ ApparelShoppingAppSolution/ApparelShoppingAppAPI/Models/DTO Models/LoginReturnDTO.cs
@@ -0,0 +1,11 @@
+﻿using ApparelShoppingAppAPI.Models.DB_Models;
+
+namespace ApparelShoppingAppAPI.Models.DTO_Models
+{
+    public class LoginReturnDTO
+    {
+        public int CustomerId { get; set; }
+        public string Token { get; set; }
+        public Role Role { get; set; }
+    }
+}
